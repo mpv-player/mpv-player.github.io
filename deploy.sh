95b252655dac144adf133d355cf40c4504bec60d
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2013-03-07 23:58:58 +0100

    add shell scripts for awesome automation

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd compiled && git add -A && git commit && git push origin gh-pages:gh-pages
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rm -r compiled
+git clone -b gh-pages git@github.com:mpv-player/mpv-player.git compiled
